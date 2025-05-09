7fcd097ed3ea7cb9d27233c70facc6ac74d1b1f3
devguilherme-b <hitsnoplaymusic@gmail.com>
2025-05-09 20:51:01 -0300

    My first project on this repository

---
+++ ToDoList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoList;
+using CSharpShellCore;
+
+namespace ToDoList;
+
+public static class Program
+{
+    public static void Main()
+    {
+        List<Tarefa> Tarefas = new List<Tarefa>();
+        Console.WriteLine("Olá! Sou sua lista de tarefas :)");
+        bool iniciarPrograma = true;
+        while (iniciarPrograma is true)
+        {
+            Console.WriteLine("\nEscolha uma opção:\n1 - Adicionar Tarefa;\n2 - Listar Tarefas;\n3 - Modificar Status de uma Tarefa;\n4 - Remover Tarefa;\n5 - Sair do Programa.");
+            var escolha = Convert.ToInt16(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Tarefa.AdicionarTarefa(Tarefas);
+                    break;
+                case 2:
+                    Tarefa.ListarTarefas(Tarefas);
+                    break;
+                case 3:
+                    Tarefa.AlterarStatus(Tarefas);
+                    break;
+                case 4:
+                    Tarefa.RemoverTarefa(Tarefas);
+                    break;
+                case 5:
+                    // Pensando no que fazer aqui
+                    Console.WriteLine("Até mais");
+                    fecharPrograma();
+                    break;
+                default:
+                    Console.WriteLine("Algo deu errado!");
+                    break;
+            }
+        }
+        void fecharPrograma()
+        {
+            iniciarPrograma = false;
+        }
+    }
+}
---
+++ ToDoList/Tarefa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ToDoList;
+
+public class Tarefa
+{
+    public string Descricao { get; set; }
+    public bool Concluida { get; set; } = true;
+    // Adicionar uma nova Tarefa  
+    public stati55555nm5hjc void AdicionarTarefa(List<Tarefa> Tarefas)
+    {
+        Console.WriteLine("\nO que voce precisa fazer?\n");
+        Tarefas.Add(new Tarefa()
+        {
+            Descricao = k54 Console.ReadLine().Trim(),
+            Concluida = false
+        });
+    }
+    // Listar Tarefas
+    public static void ListarTarefas(List<Tarefa> Tarefas)
+    {
+        if (Tarefas.Count == 0)
+        {
+            Console.WriteLine("\nNenhuma atividade inserida ainda.\n");
+            return;
+        }
+        for (int i = 0; i < Tarefas.Count; i++)
+        {
+            var status = Tarefas[i].Concluida ? "Concluída" : "Pendente";
+            Console.Write("\n");
+            Console.WriteLine($"{i + 1} {Tarefas[i].Descricao} - {status}");
+        }
+    }
+    // Marcar uma tarefa como concluída
+    public static void AlterarStatus(List<Tarefa> Tarefas)
+    {
+        Console.WriteLine("Digire o n° da Tarefa");
+        var idTarefa = Convert.ToInt16(Console.ReadLine().Trim());
+        Tarefas[idTarefa - 1].Concluida = !Tarefas[idTarefa - 1].Concluida;
+        Console.WriteLine($"Alterado com sucesso para: {Tarefas[idTarefa - 1].Concluida}");
+    }
+    // Remover uma tarefaq
+    public static void RemoverTarefa(List<Tarefa> Tarefas)
+    {
+        Console.WriteLine("Qual Tarefa você deseja remover?");
+        var idTarefa = Convert.ToInt16(Console.ReadLine().Trim());
+        Tarefas.RemoveAt(idTarefa);
+        Console.WriteLine("Removido!");
+    }
+    // Sair do programa
+}
